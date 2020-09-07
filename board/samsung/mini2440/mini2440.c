4364f346ba9d8aadd79514a487fedd02b6859110
yangwensen <fjzpyws@qq.com>
2020-09-08 00:32:47 +0800

    set cpu clock at board_early_init_f() instead of at start.s

--- board/samsung/mini2440/mini2440.c
+++ board/samsung/mini2440/mini2440.c
@@ -22,9 +22,9 @@ DECLARE_GLOBAL_DATA_PTR;
 #define M_MDIV	0xC3
 #define M_PDIV	0x4
 #define M_SDIV	0x1
-#elif (FCLK_SPEED == 1)		/* Fout = 202.8MHz */
-#define M_MDIV	0xA1
-#define M_PDIV	0x3
+#elif (FCLK_SPEED == 1)		//FCLK = 400 MHz, HCLK = 100 MHz, PCLK = 50 MH
+#define M_MDIV	92
+#define M_PDIV	0x1
 #define M_SDIV	0x1
 #endif
 
@@ -34,9 +34,9 @@ DECLARE_GLOBAL_DATA_PTR;
 #define U_M_MDIV	0xA1
 #define U_M_PDIV	0x3
 #define U_M_SDIV	0x1
-#elif (USB_CLOCK == 1)
-#define U_M_MDIV	0x48
-#define U_M_PDIV	0x3
+#elif (USB_CLOCK == 1)		//48MHz
+#define U_M_MDIV	56
+#define U_M_PDIV	0x2
 #define U_M_SDIV	0x2
 #endif
 
@@ -50,7 +50,7 @@ static inline void pll_delay(unsigned long loops)
 /*
  * Miscellaneous platform dependent initialisations
  */
-
+//by yangwensen@20200908
 int board_early_init_f(void)
 {
 	struct s3c24x0_clock_power * const clk_power =
@@ -60,16 +60,29 @@ int board_early_init_f(void)
 	/* to reduce PLL lock time, adjust the LOCKTIME register */
 	writel(0xFFFFFF, &clk_power->locktime);
 
-	/* configure MPLL */
-	writel((M_MDIV << 12) + (M_PDIV << 4) + M_SDIV,
-	       &clk_power->mpllcon);
+	writel(0, &clk_power->camdivn);		//by yangwensen@20200907
+
+	//FCLK:HCLK:PCLK = 1:4:8
+	writel((2<<1)|(1<<0), &clk_power->clkdivn);		//fclk=400mhz, fclk=100mhz, pclk=50mhz, by yangwensen@20200907
+
+	__asm__ volatile(
+		"MRC p15, 0, r0, c1, c0, 0\n"
+		"ORR r0, r0, #0xc0000000\n"
+		"MCR p15, 0, r0, c1, c0, 0\n"
+		: : :"r0"
+	);
+
+	//When you set MPLL&UPLL values, you have to set the UPLL value first and then the MPLL value. (Needs intervals approximately 7 NOP)
+	/* configure UPLL */
+	writel((U_M_MDIV << 12) + (U_M_PDIV << 4) + U_M_SDIV,
+	       &clk_power->upllcon);
 
 	/* some delay between MPLL and UPLL */
 	pll_delay(4000);
 
-	/* configure UPLL */
-	writel((U_M_MDIV << 12) + (U_M_PDIV << 4) + U_M_SDIV,
-	       &clk_power->upllcon);
+	/* configure MPLL */
+	writel((M_MDIV << 12) + (M_PDIV << 4) + M_SDIV,
+	       &clk_power->mpllcon);
 
 	/* some delay between MPLL and UPLL */
 	pll_delay(8000);
