7028ee748f5fcb633310acfe6f0ddb59dfe1eff2
yangwensen <fjzpyws@qq.com>
2020-09-16 23:48:51 +0800

    remove CONFIG_YAFFS2,bin size = 409640

--- include/configs/mini2440.h
+++ include/configs/mini2440.h
@@ -179,7 +179,9 @@
 #define CONFIG_CMD_MTDPARTS
 #define CONFIG_MTD_DEVICE
 #define CONFIG_MTD_PARTITIONS
+#if 0
 #define CONFIG_YAFFS2
+#endif
 #define CONFIG_RBTREE
 
 /* additions for new relocation code, must be added to all boards */
